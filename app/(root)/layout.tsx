518c483752a21eed13b5513992a6ea627290d6ac
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-06-26 22:29:01 +0530

    Last activity feature

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,10 +3,37 @@ import Footer from "@/components/root/Footer";
 import { ReactNode } from "react";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
+import { db } from "@/database";
+import { usersTable } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
-export default async function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const session = await auth();
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    //get the user if the last activity was today
+
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0]?.lastActivityDate === new Date().toISOString().slice(0, 10)) return;
+
+    await db
+      .update(usersTable)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(usersTable.id, session?.user?.id));
+  });
 
   return (
     <main className="root-container">
